4108cb375582167b2341e55fbabd06905d1b1f80
Shohei Miyashita <sound39music@gmail.com>
2021-02-13 17:41:41 +0900

    wip

---
+++ src/merge-sort.ts
@@ -0,0 +1,40 @@
+
+export default function sort(arr: number[]) {
+  let merged = new Array<number>(Math.ceil(arr.length / 2));
+  let width = 1;
+  while (width < arr.length) {
+    for (let i = 0; i + (width * 2) < arr.length; i += width) {
+      let l = i;
+      let r = i + width;
+
+      let j = 0;
+      do {
+        if (arr[l] < arr[r]) {
+          merged[j] = arr[l];
+          l++;
+        } else {
+          merged[j] = arr[r];
+          r++;
+        }
+        j++;
+      } while (l - i < width && r - i < width * 2);
+
+      for (; l - i < width; l++, j++) {
+        merged[j] = arr[l];
+      }
+      for (; r - i < width * 2; r++, j++) {
+        merged[j] = arr[r];
+      }
+
+      console.log(merged.slice(0, width * 2));
+      for (let k = 0; k < width * 2; k++) {
+        arr[i + k] = merged[k];
+      }
+    }
+    width *= 2;
+  }
+}
+
+const arr = [1, 2, 7, 3, 9, 5, 4, 6];
+sort(arr);
+console.log(arr);
